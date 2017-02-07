7594368e3f4127ce9cdc5e2fbdc41c28609d7bab
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-07 22:50:26 +0000

    Fixed username input. Defaults to Anonymous but accepts user input to change usernames

--- src/App.jsx
+++ src/App.jsx
@@ -9,36 +9,30 @@ class App extends Component {
     super(props);
     this.state = {
 
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: "Anonymous"}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: []
     }
     this.addMessage = this.addMessage.bind(this);
+    this.enterUsername = this.enterUsername.bind(this);
   }
 
 addMessage = (event) => {
   if(event.key == "Enter"){
-    // const newMessage = {id: (this.state.messages.length + 1), username: this.state.currentUser.name, content: event.target.value };
-    // const messages = this.state.messages.concat(newMessage);
-    // this.setState({messages: messages});
     let messageToSend = JSON.stringify({username: this.state.currentUser.name, content: event.target.value});
     this.socket.send(messageToSend);
     event.target.value = "";
   }
 }
 
+enterUsername = (event) => {
+  if(event.key == "Enter"){
+    if(event.target.value){
+      this.setState({currentUser: {name: event.target.value}});
+    }
+  }
+}
+
 componentDidMount() {
-
-  // console.log("componentDidMount <App />");
-  // setTimeout(() => {
-  //   console.log("Simulating incoming message");
-  //   // Add a new message to the list of messages in the data store
-  //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-  //   const messages = this.state.messages.concat(newMessage)
-  //   // Update the state of the app component.
-  //   // Calling setState will trigger a call to render() in App and all child components.
-  //   this.setState({messages: messages})
-  // }, 3000);
-
 
   this.socket = new WebSocket("ws://localhost:4000");
   console.log("Connected to server.");
@@ -59,7 +53,7 @@ componentDidMount() {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages}/>
-        <Chatbar addMessage={this.addMessage} currentUser={this.state.currentUser.name}/>
+        <Chatbar enterUsername={this.enterUsername} addMessage={this.addMessage} currentUser={this.state.currentUser.name}/>
       </div>
     );
   }
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -6,7 +6,7 @@ class Chatbar extends Component {
     console.log("Rendering <Chatbar/>");
     return (
       <footer className ="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" value={this.props.currentUser} />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" onKeyPress={this.props.enterUsername} defaultValue={this.props.currentUser} />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.props.addMessage}/>
       </footer>
 
