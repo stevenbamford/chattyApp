16e1cd32207b1a23c3f099cd75077ab8a8ac1748
Steven Bamford <sbamford@alumni.ubc.ca>
2017-02-07 00:30:54 +0000

    Initial build of react components

---
+++ src/MessageList.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <main className="messages">
+      </main>
+    );
+  }
+}
+export default MessageList;
